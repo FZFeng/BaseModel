4c03113dc7c0d17af7e07a578eb8b4ca32609fe2
Fabius4493 <4493fabius@163.com>
2016-06-07 17:37:21 +0800

    第一次提交

---
+++ BaseModel/BaseModel/ViewControllerTestDemo.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  BaseModel
+//
+//  Created by apple on 16/6/1.
+//  Copyright © 2016年 fabius's studio. All rights reserved.
+//  Info:测试各个类的demo
+
+#import <UIKit/UIKit.h>
+#import "FZNetworkHelper.h"
+
+@interface ViewControllerTestDemo : UIViewController
+
+- (IBAction)didBtnTest:(id)sender;
+
+@end
+
---
+++ BaseModel/BaseModel/ViewControllerTestDemo.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  BaseModel
+//
+//  Created by apple on 16/6/1.
+//  Copyright © 2016年 fabius's studio. All rights reserved.
+//
+
+#import "ViewControllerTestDemo.h"
+
+@interface ViewControllerTestDemo (){
+
+
+}
+
+@end
+
+@implementation ViewControllerTestDemo
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+//测试
+- (IBAction)didBtnTest:(id)sender {
+    
+}
+@end
